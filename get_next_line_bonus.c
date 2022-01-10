47c61bd1d8f802993c33e1cf84ffbadacf9312a4
tbousque <tbousque@student.42.fr>
2022-01-10 19:05:37 +0100

    copy bonus lol

---
+++ get_next_line_bonus.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/10 19:02:07 by tbousque          #+#    #+#             */
+/*   Updated: 2022/01/10 19:02:12 by tbousque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*verify_new_line(char **p_buf)
+{
+	char	*newline_location;
+	char	*buf;
+	char	*readed;
+	char	*new_buf;
+
+	buf = *p_buf;
+	newline_location = ft_strchr(buf, '\n');
+	if (newline_location)
+	{
+		new_buf = ft_strdup(newline_location + 1);
+		newline_location[1] = '\0';
+		readed = ft_strdup(buf);
+		free(buf);
+		buf = new_buf;
+		*p_buf = buf;
+		return (readed);
+	}
+	return (NULL);
+}
+
+char	*eof_or_error(char **saved_buf, int fd, char *buf_of_read)
+{
+	char	*readed;
+
+	readed = NULL;
+	if (ft_strlen(saved_buf[fd]))
+		readed = ft_strdup(saved_buf[fd]);
+	free(saved_buf[fd]);
+	saved_buf[fd] = NULL;
+	free(buf_of_read);
+	return (readed);
+}
+
+char	*free_buf_and_return_new_line(char **saved_buf_ptr, char *buf)
+{
+	char	*readed;
+
+	readed = verify_new_line(saved_buf_ptr);
+	free(buf);
+	return (readed);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*saved_buf[20000];
+	char		*buf;
+	ssize_t		n_read;
+	char		*readed;
+
+	if (!saved_buf[fd])
+		saved_buf[fd] = ft_strdup("");
+	readed = verify_new_line(&saved_buf[fd]);
+	if (readed)
+		return (readed);
+	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	while (1)
+	{
+		n_read = read(fd, buf, BUFFER_SIZE);
+		if (n_read == 0 || n_read == -1)
+			return (eof_or_error(saved_buf, fd, buf));
+		buf[n_read] = '\0';
+		readed = ft_strjoin(saved_buf[fd], buf);
+		free(saved_buf[fd]);
+		saved_buf[fd] = readed;
+		if (ft_strchr(buf, '\n'))
+			return (free_buf_and_return_new_line(&saved_buf[fd], buf));
+	}
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/30 20:06:28 by tbousque          #+#    #+#             */
+/*   Updated: 2022/01/10 19:04:54 by tbousque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+#endif
