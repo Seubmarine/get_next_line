5a1244186acb8884b8d65b1d9f339c936a55480b
tbousque <tbousque@student.42.fr>
2022-01-10 18:52:53 +0100

    separated gnl in 3 function

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 00:31:10 by tbousque          #+#    #+#             */
-/*   Updated: 2022/01/10 16:52:16 by tbousque         ###   ########.fr       */
+/*   Updated: 2022/01/10 18:06:07 by tbousque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,28 @@ char	*verify_new_line(char **p_buf)
 	return (NULL);
 }
 
+char	*eof_or_error(char **saved_buf, int fd, char *buf_of_read)
+{
+	char	*readed;
+
+	readed = NULL;
+	if (ft_strlen(saved_buf[fd]))
+		readed = ft_strdup(saved_buf[fd]);
+	free(saved_buf[fd]);
+	saved_buf[fd] = NULL;
+	free(buf_of_read);
+	return (readed);
+}
+
+char	*free_buf_and_return_new_line(char **saved_buf_ptr, char *buf)
+{
+	char	*readed;
+
+	readed = verify_new_line(saved_buf_ptr);
+	free(buf);
+	return (readed);
+}
+
 char	*get_next_line(int fd)
 {
 	static char	*saved_buf[20000];
@@ -51,24 +73,12 @@ char	*get_next_line(int fd)
 	{
 		n_read = read(fd, buf, BUFFER_SIZE);
 		if (n_read == 0 || n_read == -1)
-		{
-			readed = NULL;
-			if (ft_strlen(saved_buf[fd]))
-				readed = ft_strdup(saved_buf[fd]);
-			free(saved_buf[fd]);
-			saved_buf[fd] = NULL;
-			free(buf);
-			return (readed);
-		}
+			return (eof_or_error(saved_buf, fd, buf));
 		buf[n_read] = '\0';
 		readed = ft_strjoin(saved_buf[fd], buf);
 		free(saved_buf[fd]);
 		saved_buf[fd] = readed;
 		if (ft_strchr(buf, '\n'))
-		{
-			readed = verify_new_line(&saved_buf[fd]);
-			free(buf);
-			return (readed);
-		}
+			return (free_buf_and_return_new_line(&saved_buf[fd], buf));
 	}
 }
